42406a4f17a8b1527aaf9b924c63d67c87197467
DevBlinders <62160017+DevBlinders@users.noreply.github.com>
2020-03-13 23:44:11 +0100

    Create
    Create covid-19 firts version

---
+++ covid-19.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    function insertCss( code ) {
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        if (style.styleSheet) {
+            style.styleSheet.cssText = code;
+        } else {
+            style.innerHTML = code;
+        }
+        document.getElementsByTagName("head")[0].appendChild( style );
+    }
+    var codvid = '<a href="https://www.who.int/csr/disease/coronavirus_infections/es/" target="_blank" id="covid19">' +
+                    '<img src="https://www.who.int/ResourcePackages/WHO/assets/dist/images/logos/es/h-logo-blue.svg">' +
+                    '<p class="covid19__hashtag">#FrenarLaCurva</p>' +
+                '</a>';
+    document.querySelector('body').innerHTML += codvid;
+    insertCss(
+    "#covid19 {position:fixed;bottom:10px;left:10px;width: 15%;background-color: #f5f5f5;z-index: 9999999999;border: 1px solid #ccc;}" +
+    "#covid19 img {max-width: 100%;max-height: 50px;margin: 10px;}" +
+    ".covid19__hashtag {color: #555;font-size: 19px;font-weight: 800;text-align: center;}" +
+    "@media (max-width: 767px) { #covid19 {width: 100%;bottom:0;left:0;} #covid19 img, .covid19__hashtag {width: 45%; float:left;} .covid19__hashtag {margin-top: 25px;}  }"
+    );
+});
